78bfbb1944fb30cb88f4c5e244ae434ccda3e728
DreamingRaven <GeorgeRavenNR@outlook.com>
2020-04-08 14:29:22 +0100

    Updated setup.py names
    Changed names to python-ezdb

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
-
 # @Author: George Onoufriou <archer>
 # @Date:   2018-09-05
 # @Filename: setup.py
 # @Last modified by:   archer
-# @Last modified time: 2019-08-17
+# @Last modified time: 2020-04-08T14:29:09+01:00
 # @License: Please see LICENSE file in project root
 
 import subprocess
@@ -13,7 +12,7 @@ from setuptools import setup, find_packages, find_namespace_packages
 
 
 # getting version from git as this is vcs
-# git describe --long | sed 's/\([^-]*-\)g/r\1/;s/-/./g
+# below equivelant: git describe --long | sed 's/\([^-]*-\)g/r\1/;s/-/./g
 git_describe = subprocess.Popen(["git", "describe", "--long"],
                                 stdout=subprocess.PIPE)
 version_num = subprocess.check_output(["sed", r"s/\([^-]*-\)g/r\1/;s/-/./g"],
@@ -28,13 +27,13 @@ with open("README.md", "r") as fh:
     readme = fh.read()
 
 setup(
-    name="pyrtd",
+    name="python-ezdb",
     version=str(version_git),
     description="Template repository.",
     long_description=readme,
     long_description_content_type="text/markdown",
     author="George Onoufriou",
-    url="https://github.com/DreamingRaven/pyrtd",
+    url="https://github.com/DreamingRaven/python-ezdb",
     packages=find_namespace_packages(),
     scripts=['pyrtd'],
     install_requires=requirements
