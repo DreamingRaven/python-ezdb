e724047f27e04193b2474e58d2900bd3ebb7ac62
DreamingRaven <GeorgeRavenNR@outlook.com>
2020-05-01 22:20:55 +0100

    Added version function to setup.py
    This function will return the git version of source, still considering
    alternatives for windows or non git/ sed machines.
    
    We can either use import git to get around sed requirement but is
    difficult to get into the desired format we want, and requires another
    dependancy prior to being able to run setup which could cause headaches
    especially if they are windows users, they are probably not used to this
    kind of thing. Other possibility is subprocess but we cant guarantee
    that these commands will succeed either.

--- setup.py
+++ setup.py
@@ -7,18 +7,38 @@
 # @Last modified time: 2020-04-08T14:39:26+01:00
 # @License: Please see LICENSE file in project root
 
+import os
 import subprocess
 from setuptools import setup, find_packages, find_namespace_packages
 
+import pathlib
 
-# getting version from git as this is vcs
-# below equivelant: git describe --long | sed 's/\([^-]*-\)g/r\1/;s/-/./g
-git_describe = subprocess.Popen(["git", "describe", "--long"],
-                                stdout=subprocess.PIPE)
-version_num = subprocess.check_output(["sed", r"s/\([^-]*-\)g/r\1/;s/-/./g"],
-                                      stdin=git_describe.stdout)
-git_describe.wait()
-version_git = version_num.decode("ascii").strip()
+
+def get_gitVersion():
+    """Get the version from git describe in archlinux format."""
+    try:
+        # getting version from git as this is vcs
+        # below equivelant or achlinux versioning scheme:
+        # git describe --long | sed 's/\([^-]*-\)g/r\1/;s/-/./g
+        git_describe = subprocess.Popen(
+            ["git", "describe", "--long"],
+            stdout=subprocess.PIPE)
+        version_num = subprocess.check_output(
+            ["sed", r"s/\([^-]*-\)g/r\1/;s/-/./g"],
+            stdin=git_describe.stdout)
+        git_describe.wait()
+        version_git = version_num.decode("ascii").strip()
+
+    except subprocess.CalledProcessError:
+        # for those who do not have git or sed availiable (probably non-linux)
+        # this is tricky to handle, lots of suggestions exist but none that
+        # neither require additional library or subprocessess
+        version_git = "0.0.1"  # for now we will provide a number for you
+    return version_git
+
+
+version = get_gitVersion()
+print("version: ", version)
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
@@ -31,7 +51,7 @@ print("namespace packages:", namespace_packages)
 
 setup(
     name="python-ezdb",
-    version=str(version_git),
+    version=str(version),
     description="Template repository.",
     long_description=readme,
     long_description_content_type="text/markdown",
